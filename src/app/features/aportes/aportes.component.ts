74af0914d5192584ed333a512ae1063785bd0875
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-05-17 15:18:12 -0400

    aportes page

--- src/app/features/aportes/aportes.component.ts
+++ src/app/features/aportes/aportes.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-aportes',
   imports: [NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    RouterModule
   ],
   templateUrl: './aportes.component.html',
   styleUrl: './aportes.component.css'
