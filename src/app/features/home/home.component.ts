c439f970224aa12e04709faa4c3c04331bcc7ec2
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-05-18 22:29:39 -0400

    actualización home y componentes

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { CarrouselComponent } from '../../shared/components/carrousel/carrousel.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    FooterComponent
+    FooterComponent, 
+    NavbarComponent,
+    CarrouselComponent,
+    MatButtonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  churchImages = [
+    {
+      url: 'assets/parro1.jpg',
+      alt: 'Church interior with stained glass',
+      title: 'Te invitamos a participar de nuestra comunidad',
+      title2: 'Martes a Sábado: 20:00 Domingos 12:00 y 20:00'
+    },
+    {
+      url: 'assets/parro2.jpg',
+      alt: 'Church exterior view',
+      title: 'Horario de Misa',
+      title2: 'Martes a Sábado: 20:00 Domingos 12:00 y 20:00'
+    },
+    {
+      url: 'assets/parro3.jpg',
+      alt: 'Community gathering',
+      title: 'Community Events',
+      title2: 'Martes a Sábado: 20:00 Domingos 12:00 y 20:00'
+    }
+  ];
 }
